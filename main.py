f32c033516f5bd45040eb905b0e2b35abaa8a6df
Lucas Vinh Tran <lucasvinhtran@gmail.com>
2021-10-06 09:55:33 +0800

    update test_data for data check

--- main.py
+++ main.py
@@ -63,10 +63,17 @@ def go(config: DictConfig):
             )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,16 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    '''
+    Checks that the size of the dataset is reasonable (not too small, not too large)
+    '''
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    '''
+    Checks that the price range is between min_price and max_price
+    '''
+    idx = data['price'].between(min_price, max_price)
+    assert len(data[~idx]) == 0
