510e509263d0579dd692dc60cfa14fdde5028b45
Marcus Holmgren <marcus.holmgren1@gmail.com>
2021-10-17 15:00:31 +0200

    feat: add test for price range.

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -4,7 +4,6 @@ import scipy.stats
 
 
 def test_column_names(data):
-
     expected_colums = [
         "id",
         "name",
@@ -31,7 +30,6 @@ def test_column_names(data):
 
 
 def test_neighborhood_names(data):
-
     known_names = ["Bronx", "Brooklyn", "Manhattan", "Queens", "Staten Island"]
 
     neigh = set(data['neighbourhood_group'].unique())
@@ -60,8 +58,16 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
 def test_row_count(data):
+    """
+    Ensure that there is enough rows in the data set.
+    """
     assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    """
+    Ensure that the data set column price have sensible values within the given thresholds
+    """
+    assert data['price'].between(min_price,
+                                 max_price).all(), f"Expected price to be between {min_price} and {max_price}"
