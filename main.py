7cf448f084ddc693ced5287976a972e23f821ff3
Marcus Holmgren <marcus.holmgren1@gmail.com>
2021-10-17 12:46:24 +0200

    Basic cleaning (#3)
    * refactor: basic_cleaning with new arguments
    
    * feat: basic cleaning complete step

--- main.py
+++ main.py
@@ -16,14 +16,13 @@ _steps = [
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
-#    "test_regression_model"
+    #    "test_regression_model"
 ]
 
 
 # This automatically reads in the configuration
 @hydra.main(config_name='config')
 def go(config: DictConfig):
-
     # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
@@ -56,9 +55,12 @@ def go(config: DictConfig):
                 os.path.join(root_path, "src", "basic_cleaning"),
                 "main",
                 parameters={
-                    "parameter1": "a",
-                    "parameter2": "b",
-                    "parameter3": "c"
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_sample",
+                    "output_description": "Data with outliers and null values removed",
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
                 }
             )
 
@@ -88,7 +90,6 @@ def go(config: DictConfig):
             )
 
         if "train_random_forest" in active_steps:
-
             # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,58 +1,99 @@
 #!/usr/bin/env python
 """
-Performs basic cleaning on the data and save the results in Weights & Biases
+Download from Weights & Biases the raw dataset and apply some basic data cleaning,
+exporting the result to a new artifact in W&B
 """
+import os
 import argparse
 import logging
 import wandb
+import pandas as pd
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
-def go(args):
+def go(args: argparse.Namespace):
 
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
+    logger.info("Download input artifact %s", args.input_artifact)
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
-    print(f"Hello from basic_cleaning: {args.parameter1}, {args.parameter2}, {args.parameter3}")
+    df = pd.read_csv(artifact_local_path)
+    min_price = args.min_price
+    max_price = args.max_price
+    logger.info("Remove price outlier. Use range min: %.2f  -- max: %.2f", min_price, max_price)
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    df.to_csv(args.output_artifact)
+
+    # upload to W&B
+    logger.info("Upload output artifact %s of type %s", args.output_artifact, args.output_type)
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file(args.output_artifact)
+    run.log_artifact(artifact)
+
+    os.remove(args.output_artifact)
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="This step cleans data")
-
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
     parser.add_argument(
-        "--parameter1", 
-        type=str, ## INSERT TYPE HERE: str, float or int,
-        help=" ## INSERT DESCRIPTION HERE",
+        "--input_artifact",
+        type=str,
+        help="the input artifact",
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
-        type=str, ## INSERT TYPE HERE: str, float or int,
-        help=" ## INSERT DESCRIPTION HERE",
+        "--output_artifact",
+        type=str,
+        help="the name for the output artifact",
         required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
-        type=str, ## INSERT TYPE HERE: str, float or int,
-        help=" ## INSERT DESCRIPTION HERE",
+        "--output_type",
+        type=str,
+        help="the type for the output artifact",
         required=True
     )
 
+    parser.add_argument(
+        "--output_description",
+        type=str,
+        help="a description for the output artifact",
+        required=True
+    )
 
-    args = parser.parse_args()
+    parser.add_argument(
+        "--min_price",
+        type=float,
+        help="the minimum price to consider",
+        required=True
+    )
 
-    go(args)
+    parser.add_argument(
+        "--max_price",
+        type=float,
+        help="the maximum price to consider",
+        required=True
+    )
+
+    arguments = parser.parse_args()
+
+    go(arguments)
