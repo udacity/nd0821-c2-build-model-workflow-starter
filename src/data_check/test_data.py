aa47b4b189bf58af712dafde11baaf79d423d22b
Laura Calcagni <l.calcagni@gmail.com>
2021-10-25 19:25:27 -0300

    Add tests

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,16 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    """
+    Test proper row count
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Test if price range is between min and max prices allowed
+    """
+    items_ok = data['price'].between(min_price, max_price).shape[0]
+    assert data.shape[0] == items_ok
