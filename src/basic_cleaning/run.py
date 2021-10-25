bfcfe2d82f4a3605eeebc98a8d79a26553169268
Laura Calcagni <l.calcagni@gmail.com>
2021-10-25 18:34:20 -0300

    Add dependencies

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -12,17 +12,36 @@ logger = logging.getLogger()
 
 
 def go(args):
-
+    
+    logger.info("Initialising wandb")
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
-    # Download input artifact. This will also log that this script is using this
-    # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    logger.info(f'Getting data')
+    local_path = wandb.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(local_path)
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    logger.info('Cleaning data')
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    logger.info(f'Saving output')
+    df.to_csv(args.output_artifact, index=False)
+
+    logger.info(f'Sending result to wandb')
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file(args.output_artifact)
+    run.log_artifact(artifact)
+
+    logger.info(f'Terminate wandb run')
+    run.finish()
 
 
 if __name__ == "__main__":
