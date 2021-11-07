516e576f7dee1b3db6c1ee01a9c128574824a542
Elvin Gomez <elvin.gomez@inari.io>
2021-11-07 21:04:45 +0100

    best performing model release

--- main.py
+++ main.py
@@ -113,12 +113,14 @@ def go(config: DictConfig):
             )
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": f"{config['modeling']['export_artifact']}:prod",
+                    "test_dataset": "test_data.csv:latest"
+                }
+            )
 
 
 if __name__ == "__main__":
