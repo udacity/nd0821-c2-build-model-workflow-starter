d567701ea2901bf2c4ee91a35a890c331cc3da52
G-Hung <geoffrey12177@gmail.com>
2021-11-14 17:01:32 -0800

    data splitting

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def go(config: DictConfig):
 
 
         if "data_check" in active_steps:
-            # Basic cleaning
+            # Data Check
             _ = mlflow.run(
                 os.path.join(root_path, "src", "data_check"),
                 "main",
@@ -81,10 +81,17 @@ def go(config: DictConfig):
 
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            # Data Check
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config['modeling']['test_size'],
+                    "random_seed": config['modeling']['random_seed'],
+                    "stratify_by": config['modeling']['stratify_by'],
+                },
+            )
 
         if "train_random_forest" in active_steps:
 
