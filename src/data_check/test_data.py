18aa0c2aec6364a0304e3c1d0c8333d0dbcc5c62
Victor_Casarotto <victor_casarotto@mckinsey.com>
2021-12-21 11:51:37 -0300

    Added data_check module - running into pytest path error

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,10 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    idx = data['price'].between(min_price, max_price)
+    assert np.sum(~idx) == 0
