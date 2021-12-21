2290b0904198ac66bad09a225f81f09d85312628
Victor_Casarotto <victor_casarotto@mckinsey.com>
2021-12-21 17:07:09 -0300

    Added filter for lat/lon

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,6 +30,10 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
+    # Remove lat/lon that are outside boundaries
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Convert last_review column type from str to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
     logger.info('Completed cleaning processing')
