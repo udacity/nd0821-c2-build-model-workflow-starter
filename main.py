72f085c7dcf74e34931e4d3b022224906917756f
Victor_Casarotto <victor_casarotto@mckinsey.com>
2021-12-21 16:02:17 -0300

    Added training and testing step to main and edited run files

--- main.py
+++ main.py
@@ -76,13 +76,18 @@ def go(config: DictConfig):
             )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_split"),
+                "main",
+                parameters={
+                    'input': 'clean_sample.csv:latest',
+                    'test_size': config['modeling']['test_size'],
+                    'random_seed': config['modeling']['random_seed'],
+                    'stratify_by': config['modeling']['stratify_by'],
+                },
+            )
 
         if "train_random_forest" in active_steps:
-
             # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
@@ -91,19 +96,29 @@ def go(config: DictConfig):
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    'trainval_artifact': 'trainval_data.csv:latest',
+                    'val_size': config['modeling']['val_size'],
+                    'random_seed': config['modeling']['random_seed'],
+                    'stratify_by': config['modeling']['stratify_by'],
+                    'rf_config': rf_config,
+                    'max_tfidf_features': config['modeling']['max_tfidf_features'],
+                    'output_artifact': config['modeling']['export_artifact'],
+                },
+            )
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                 os.path.join(hydra.utils.get_original_cwd(), "src", "test_regression_model"),
+                "main",
+                parameters={
+                    'mlflow_model': f"{config['modeling']['export_artifact']}:prod",
+                    'test_dataset': 'test_data.csv:latest',
+                },
+            )
 
 
 if __name__ == "__main__":
