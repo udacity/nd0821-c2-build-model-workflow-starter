2561942c9f9f7856dbe33dee04f3ffcf4d6fd7c3
Victor_Casarotto <victor_casarotto@mckinsey.com>
2021-12-21 16:55:22 -0300

    Ran test model module - success

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def go(config: DictConfig):
 
         if "test_regression_model" in active_steps:
             _ = mlflow.run(
-                 os.path.join(hydra.utils.get_original_cwd(), "src", "test_regression_model"),
+                 os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
                 "main",
                 parameters={
                     'mlflow_model': f"{config['modeling']['export_artifact']}:prod",
