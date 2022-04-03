50216d717a8666e3eeda4cd469d1770bc2d237c0
Felipe Machado <felipe.krids@gmail.com>
2022-04-03 19:14:10 -0300

    Created the test regression model pipeline on the main file.

--- main.py
+++ main.py
@@ -109,17 +109,16 @@ def go(config: DictConfig):
                     "output_artifact": "random_forest_export"
                 }
             )
-
-            pass
-
+            
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
-
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                }
+            )
 
 if __name__ == "__main__":
     go()
