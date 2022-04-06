8f7c42edd5b9246c2cbd40e987dc2919db6ff0ae
Heber Trujillo <heber.trj.urt@gmail.com>
2022-04-06 08:32:47 +0200

    [UPD] basic cleaning component implemented in main script.

--- main.py
+++ main.py
@@ -49,10 +49,23 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+
+            _ = mlflow.run(
+                uri=os.path.join(
+                    hydra.utils.get_original_cwd(),
+                    "src",
+                    "basic_cleaning"
+                ),
+                entry_point="main",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "cleaned_sample.csv",
+                    "output_type": "clean_data",
+                    "output_description": "Data after preprocessing",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )
 
         if "data_check" in active_steps:
             ##################
