f88f8575d0308cec29db55b0d5fd8c45a0df8358
Ashraf Al Gumaei <sharafah@vcu.edu>
2022-04-23 15:52:12 -0400

    Added data split step

--- main.py
+++ main.py
@@ -76,10 +76,16 @@ def go(config: DictConfig):
             )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"]
+                },
+            )
 
         if "train_random_forest" in active_steps:
 
