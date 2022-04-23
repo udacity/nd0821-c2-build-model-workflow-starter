52358039a4983636366709865e5023e72fe4ed20
Ashraf Al Gumaei <sharafah@vcu.edu>
2022-04-23 15:23:55 -0400

    Added data check step

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,9 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    assert data['price'].between(min_price, max_price).all()
