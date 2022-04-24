c40e5d35a32392ba43908a69ea59aec47ce3c775
Ashraf Al Gumaei <sharafah@vcu.edu>
2022-04-23 23:38:07 -0400

    Added test regression step

--- main.py
+++ main.py
@@ -111,12 +111,14 @@ def go(config: DictConfig):
             )
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                },
+            )
 
 
 if __name__ == "__main__":
